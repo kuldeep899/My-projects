fee6b872efa83df4128b6a769becd4ed09f94367
Kuldeep Sahu <kuldeepsahu037@gmail.com>
2022-07-10 05:18:12 +0530

    code and circuit changed

--- Robotics_Projects/Fire-fighting-Robot/Fire_Fighting_Robot/Fire_Fighting_Robot.ino
+++ Robotics_Projects/Fire-fighting-Robot/Fire_Fighting_Robot/Fire_Fighting_Robot.ino
@@ -1,54 +1,44 @@
-
-#include <Servo.h>  //include servo.h library
+/*------ Arduino Fire Fighting Robot Code----- */
+#include <Servo.h>
 Servo myservo;
-
 int pos = 0;
 boolean fire = false;
 
-#define Left 10      // left sensor
-#define Right 12    // right sensor
-#define Forward 11   //front sensor
+/*-------defining Inputs------*/
+#define Left_S 9      // left sensor
+#define Right_S 10      // right sensor
+#define Forward_S 8 //forward sensor
 
+/*-------defining Outputs------*/
 #define LM1 2       // left motor
-#define LM2 4       // left motor
-#define enL 3
-#define RM1 7       // right motor
-#define RM2 8       // right motor
-#define enR 9
-#define pump 13
+#define LM2 3       // left motor
+#define RM1 4       // right motor
+#define RM2 5       // right motor
+#define pump 6
 
 void setup()
 {
-  pinMode(Left, INPUT);
-  pinMode(Right, INPUT);
-  pinMode(Forward, INPUT);
+  pinMode(Left_S, INPUT);
+  pinMode(Right_S, INPUT);
+  pinMode(Forward_S, INPUT);
   pinMode(LM1, OUTPUT);
   pinMode(LM2, OUTPUT);
-  pinMode(enL, OUTPUT);
   pinMode(RM1, OUTPUT);
   pinMode(RM2, OUTPUT);
-  pinMode(enR, OUTPUT);
   pinMode(pump, OUTPUT);
 
-  myservo.attach(6);
+  myservo.attach(11);
   myservo.write(90);
-
-  
 }
 
 void put_off_fire()
 {
-
   delay (500);
-
   digitalWrite(LM1, HIGH);
   digitalWrite(LM2, HIGH);
   digitalWrite(RM1, HIGH);
   digitalWrite(RM2, HIGH);
-
-  digitalWrite(pump, HIGH);
-  delay(500);
-
+  digitalWrite(pump, HIGH); delay(500);
   for (pos = 50; pos <= 130; pos += 1) {
     myservo.write(pos);
     delay(10);
@@ -57,30 +47,25 @@ void put_off_fire()
     myservo.write(pos);
     delay(10);
   }
-
   digitalWrite(pump, LOW);
   myservo.write(90);
-
   fire = false;
 }
-
 void loop()
 {
-  analogWrite(enR, 200);
-  analogWrite(enL, 200);
   myservo.write(90); //Sweep_Servo();
-
-  if (digitalRead(Left) == 1 && digitalRead(Right) == 1 && digitalRead(Forward) == 1)
+  if (digitalRead(Left_S) == 1 && digitalRead(Right_S) == 1 && digitalRead(Forward_S) == 1) //If Fire not detected all sensors are zero
   {
-
+    //Do not move the robot
     digitalWrite(LM1, HIGH);
     digitalWrite(LM2, HIGH);
     digitalWrite(RM1, HIGH);
     digitalWrite(RM2, HIGH);
   }
 
-  else if (digitalRead(Forward) == 0)
+  else if (digitalRead(Forward_S) == 0) //If Fire is straight ahead
   {
+    //Move the robot forward
     digitalWrite(LM1, HIGH);
     digitalWrite(LM2, LOW);
     digitalWrite(RM1, HIGH);
@@ -88,23 +73,24 @@ void loop()
     fire = true;
   }
 
-  else if (digitalRead(Left) == 0)
+  else if (digitalRead(Left_S) == 0) //If Fire is to the left
   {
+    //Move the robot left
     digitalWrite(LM1, HIGH);
     digitalWrite(LM2, LOW);
     digitalWrite(RM1, HIGH);
     digitalWrite(RM2, HIGH);
   }
 
-  else if (digitalRead(Right) == 0)
+  else if (digitalRead(Right_S) == 0) //If Fire is to the right
   {
+    //Move the robot right
     digitalWrite(LM1, HIGH);
     digitalWrite(LM2, HIGH);
     digitalWrite(RM1, HIGH);
     digitalWrite(RM2, LOW);
   }
-
-  delay(300);//change this value to increase the distance
+  delay(300);
 
   while (fire == true)
   {
